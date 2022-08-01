a168610561c9cbad6ea8a8c3d690f509cda92d1b
Sabina-Pandey <sabinapandey700@gmail.com>
2022-08-01 13:41:08 +0545

    version1

---
+++ adddata.php
@@ -0,0 +1,13 @@
+<?php
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+        $gender = $_POST['gender'];
+        $email = $_POST['email'];
+        $number = $_POST['number'];
+
+        echo $name; 
+        echo $age;
+        echo $gender;
+        echo $email;
+        echo $number;
+?>
